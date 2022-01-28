8fab872baadf5ea7b9f046f59cde2d16da3cb011
anyahush <anyahush@gmail.com>
2022-01-28 13:25:22 +0000

    Fix connection error to MongoDB

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
-from flask import(
-    Flask, flash, render_template, redirect, request, session, url_for)
+from flask import (
+    Flask, flash, render_template, 
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -11,7 +12,7 @@ if os.path.exists("env.py"):
 app = Flask(__name__)
 
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
-app.config["MONGO_URI"] = os.environ.get ("MONGO_URI")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 
@@ -26,6 +27,43 @@ def get_recipes():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    """ register view, when form submitted database
+    is updated
+    """
+    if request.method == "POST":
+        # checks if username is already taken
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            # if username is taken, message displayed and reloads page
+            flash("Username already exists")
+            return redirect(url_for("register"))
+
+        if request.form.get("password") != request.form.get(
+                "confirm-password"):
+            # checks if confirmed password matches original password
+            # If confirmation
+            # not met, flash message displayed and reloads page
+            flash("Your password did not match, please try again")
+            return redirect(url_for("register"))
+        
+        register_user = {
+            # gets user information from registration form
+            "username": request.form.get("username").lower(),
+            "first-name": request.form.get("first-name"),
+            "surname": request.form.get("surname"),
+            "email": request.form.get("email"),
+            "password": generate_password_hash(request.form.get("password")),
+            "admin": bool("")
+        }
+        # new user created in db
+        mongo.db.users.insert_one(register_user)
+
+        # put the new user into 'session' cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration is successful!")
+
     return render_template("register.html")
 
 
