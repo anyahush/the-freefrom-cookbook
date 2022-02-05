bbb02e468e98b363d4e071b9c45c5630966a9cd4
anyahush <anyahush@gmail.com>
2022-02-05 19:01:47 +0000

    Dynamically generate category and allergen select lists

--- app.py
+++ app.py
@@ -167,7 +167,12 @@ def saved_recipes():
 
 @app.route("/create_recipe")
 def create_recipe():
-    return render_template("create_recipe.html")
+    """
+    Allow users to create a recipe and save
+    """
+    categories = mongo.db.categories.find().sort("category_name", 1)
+    allergens = mongo.db.allergens.find().sort("allergen_name", 1)
+    return render_template("create_recipe.html", categories=categories, allergens=allergens)
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
