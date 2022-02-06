d60aebe009807ef1c1cc5ab8f6755c14adc60ab7
anyahush <anyahush@gmail.com>
2022-02-06 10:53:56 +0000

    Add form validation and uniform styling to create recipe form

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,37 @@
  $(document).ready(function(){
     $('.sidenav').sidenav();
     $('select').formSelect();
-  });
+
+    // used from Code Institute Task Manager full details in README
+    validateMaterializeSelect();
+    function validateMaterializeSelect() {
+      let classValid = { "border-bottom": "1px solid #4caf50", "box-shadow": "0 1px 0 0 #4caf50" };
+      let classInvalid = { "border-bottom": "1px solid #f44336", "box-shadow": "0 1px 0 0 #f44336" };
+      if ($("select.validate").prop("required")) {
+          $("select.validate").css({ "display": "block", "height": "0", "padding": "0", "width": "0", "position": "absolute" });
+    }
+    $(".select-wrapper input.select-dropdown").on("focusin", function () {
+        $(this).parent(".select-wrapper").on("change", function () {
+            if ($(this).children("ul").children("li.selected:not(.disabled)").on("click", function () { })) {
+                $(this).children("input").css(classValid);
+            }
+        });
+    }).on("click", function () {
+        if ($(this).parent(".select-wrapper").children("ul").children("li.selected:not(.disabled)").css("background-color") === "rgba(0, 0, 0, 0.03)") {
+            $(this).parent(".select-wrapper").children("input").css(classValid);
+        } else {
+            $(".select-wrapper input.select-dropdown").on("focusout", function () {
+                if ($(this).parent(".select-wrapper").children("select").prop("required")) {
+                    if ($(this).css("border-bottom") != "1px solid rgb(76, 175, 80)") {
+                        $(this).parent(".select-wrapper").children("input").css(classInvalid);
+                    }
+                }
+            });
+        }
+    });
+  }
+});
+//End of CI code
 
 // Add list item to ingredients list when '+' button hit
 // Used from https://github.com/RussOakham/wanderlust-recipes/blob/master/static/js/script.js
@@ -44,3 +74,5 @@ $('#method_step').on("click", ".remove-list-item", function (event) {
   $(this).parent().remove();
 });
 // End of Wanderlust code 
+
+
