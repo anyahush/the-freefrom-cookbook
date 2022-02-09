2c7b85c83d0c0b27a6b397d521493ba71baec36c
anyahush <anyahush@gmail.com>
2022-02-09 18:33:52 +0000

    Create route decorator and update nav links for about page

--- app.py
+++ app.py
@@ -27,6 +27,10 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
 @app.route("/get_recipes")
 def get_recipes():
     """
